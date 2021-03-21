9b8436653350c7d288e595df21f8381e46189860
ticn <61242872+ticn@users.noreply.github.com>
2021-03-21 20:02:18 +0800

    Create upload.sh

---
+++ upload.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#============================================================
+
+DOWNLOAD_PATH='/root/Download'
+FILE_PATH=$3                                          # Aria2传递给脚本的文件路径。BT下载有多个文件时该值为文件夹内第一个文件，如/root/Download/a/b/1.mp4
+TASK_INFO() {
+    echo -e "
+-------------------------- [TASK INFO] --------------------------
+Download path: ${DOWNLOAD_PATH}
+File path: ${FILE_PATH}
+-------------------------- [TASK INFO] --------------------------
+"
+}
+
+#python3 
+nohup python3 ./ali.py u $3 /root
